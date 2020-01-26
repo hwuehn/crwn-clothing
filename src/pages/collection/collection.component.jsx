17328f628fc266a92b1b59635059aa7c92d9dc89
Henning Wuehn <h.wuehn@posteo.de>
2020-01-26 13:39:23 +0100

    fixed bug, shop details are now listed

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -11,9 +11,9 @@ const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
   return (
     <div className='collection-page'>
-      <h1 className='title'>{title}</h1>
+      <h2 className='title'>{title}</h2>
       <div className='items'>
-        {items.map((item) => (
+        {items.map(item => (
           <CollectionItem key={item.id} item={item} />
         ))}
       </div>
@@ -22,7 +22,7 @@ const CollectionPage = ({ collection }) => {
 };
 
 const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionID)(state)
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
 });
 
 export default connect(mapStateToProps)(CollectionPage);
